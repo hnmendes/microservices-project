698513ce555c521d0f80b5b35ef9639a949e86d0
unknown <henriquenunesti@gmail.com>
2021-11-11 23:37:40 -0300

    #ordering-service - Creating all needed subfolders.  - Also creating the Queries part from CQRS

---
+++ src/Ordering.Service/Ordering.Application/Features/Orders/Queries/GetOrderList/GetOrderListQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrderList
+{
+    public class GetOrderListQuery : IRequest<List<OrderViewModel>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+
+    }
+}
---
+++ src/Ordering.Service/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using Ordering.Domain.Entities.Base;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}

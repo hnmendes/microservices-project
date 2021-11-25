1a1384dcf1e00ff609cf2a545031f8f5e71bd956
unknown <henriquenunesti@gmail.com>
2021-11-25 02:34:13 -0300

    #message-part - adding rabbitmq

---
+++ src/BuildingBlocks/EventBus.Messages/Events/BaseEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class BaseEvent
+    {
+        public BaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        public BaseEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreatedDate { get; private set; }
+    }
+}
